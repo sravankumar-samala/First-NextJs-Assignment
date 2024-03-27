deac4ee4465d0a38a4ddd6939db8cb68e844ac8a
sravankumar-samala <sravankumarsamala20@gmail.com>
2024-03-27 15:55:48 +0530

    Updated dark mode toggler position

--- src/components/darkModeToggler.tsx
+++ src/components/darkModeToggler.tsx
@@ -18,7 +18,7 @@ export default function DarkModeToggler({
       <label
         htmlFor="check"
         className="inline-block peer-checked:bg-green-500 peer-checked:before:left-[calc(100%-22px)]
-         absolute bottom-9 right-9 w-[50px] h-[26px] border-solid border-2 border-black rounded-3xl bg-slate-200 cursor-pointer
+         fixed bottom-9 right-9 w-[50px] h-[26px] border-solid border-2 border-black rounded-3xl bg-slate-200 cursor-pointer
          transition-all duration-500 before:absolute before:bg-slate-900 before:top-[1px]
          before:left-[2px] before:rounded-3xl before:w-[20px] before:h-[20px] before:transition-all before:duration-200"
       ></label>
